dd9c47b923d8fe0d72538e12d29fe67028822159
soulteary <soulteary@gmail.com>
2023-01-06 00:33:18 +0800

    test: full coverage for generator/generator

---
+++ internal/generator/generator_test.go
@@ -0,0 +1,27 @@
+package generator_test
+
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/soulteary/certs-maker/internal/define"
+	"github.com/soulteary/certs-maker/internal/generator"
+)
+
+func TestGenerate(t *testing.T) {
+	define.APP_FOR_K8S = false
+	define.CERT_DOMAINS = append(define.CERT_DOMAINS, "abc.com")
+	define.APP_OUTPUT_DIR = "./"
+
+	generator.Generate()
+
+	if _, err := os.Stat("./abc.com.key"); errors.Is(err, os.ErrNotExist) {
+		t.Fatal("test MakeCerts failed")
+	}
+	os.Remove("./abc.com.key")
+	if _, err := os.Stat("./abc.com.conf"); errors.Is(err, os.ErrNotExist) {
+		t.Fatal("test MakeCerts failed")
+	}
+	os.Remove("./abc.com.conf")
+}
