9ce5683e2fc44d9349c62e767a5ee157192e71c5
Su Yang <soulteary@gmail.com>
2024-09-02 16:01:14 +0800

    feat: add output certs format

--- internal/define/template.go
+++ internal/define/template.go
@@ -34,11 +34,11 @@ extendedKeyUsage = serverAuth
 subjectAltName = @alt_names
 `
 
-const GENERATE_CMD_TPL = "openssl req -x509 -newkey rsa:2048 -keyout ${file}.key -out ${file}.crt -days 3650 -nodes -config ${file}.conf"
+const GENERATE_CMD_TPL = "openssl req -x509 -newkey rsa:2048 -keyout ${file}.pem.key -out ${file}.pem.crt -days 3650 -nodes -config ${file}.conf"
 const GENERATE_CMD_PLACEHOLDER = "${file}"
 
 const GENERATE_FOR_FF_STEP1 = "openssl genrsa -out ${file}.rootCA.key 2048"
 const GENERATE_FOR_FF_STEP2 = "openssl req -utf8 -x509 -new -nodes -key ${file}.rootCA.key -sha256 -days 3650 -out ${file}.rootCA.pem -config ${file}.conf"
-const GENERATE_FOR_FF_STEP3 = "openssl genrsa -out ${file}.key 2048"
-const GENERATE_FOR_FF_STEP4 = "openssl req -utf8 -new -key ${file}.key -out ${file}.csr -config ${file}.conf"
-const GENERATE_FOR_FF_STEP5 = "openssl x509 -req -in ${file}.csr -CA ${file}.rootCA.pem -CAkey ${file}.rootCA.key -CAcreateserial -out ${file}.crt -days 3650 -sha256"
+const GENERATE_FOR_FF_STEP3 = "openssl genrsa -out ${file}.pem.key 2048"
+const GENERATE_FOR_FF_STEP4 = "openssl req -utf8 -new -key ${file}.pem.key -out ${file}.pem.csr -config ${file}.conf"
+const GENERATE_FOR_FF_STEP5 = "openssl x509 -req -in ${file}.pem.csr -CA ${file}.rootCA.pem -CAkey ${file}.rootCA.key -CAcreateserial -out ${file}.pem.crt -days 3650 -sha256"
