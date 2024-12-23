7319c642cfbefd5f0b68e448a5358a72d6eb5c66
Su Yang <soulteary@gmail.com>
2024-12-24 04:32:59 +0800

    feat: support ipv4 and ipv6 addrs

--- internal/fn/domain.go
+++ internal/fn/domain.go
@@ -1,6 +1,8 @@
 package fn
 
 import (
+	"fmt"
+	"net"
 	"regexp"
 	"strings"
 )
@@ -18,13 +20,25 @@ func GetUniqDomains(s []string) []string {
 }
 
 func GetDomainsByString(input string) (result []string) {
-	var re = regexp.MustCompile(`^([\.\w\*\-\_]+(\,)?){1,}$`)
-	if len(re.FindAllString(input, -1)) > 0 {
-		domains := strings.Split(input, ",")
-		for _, domain := range domains {
-			s := strings.TrimSpace(domain)
-			if len(s) > 0 {
-				result = append(result, strings.ToLower(domain))
+	domainPattern := `([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}`
+	ipv4Pattern := `((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)`
+	ipv6Pattern := `(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))`
+	combinedPattern := fmt.Sprintf(`^(%s|%s|%s)$`, domainPattern, ipv4Pattern, ipv6Pattern)
+	re := regexp.MustCompile(combinedPattern)
+
+	addresses := strings.Split(input, ",")
+
+	for _, addr := range addresses {
+		addr = strings.TrimSpace(addr)
+		if len(addr) == 0 {
+			continue
+		}
+
+		if re.MatchString(addr) {
+			if strings.Contains(addr, ":") || net.ParseIP(addr) != nil {
+				result = append(result, addr)
+			} else {
+				result = append(result, strings.ToLower(addr))
 			}
 		}
 	}
