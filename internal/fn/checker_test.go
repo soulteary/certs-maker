548a147dd799d866f3a3ba8f94db517e50f51808
Su Yang <soulteary@gmail.com>
2025-03-07 16:47:41 +0800

    chore: update license

--- internal/fn/checker_test.go
+++ internal/fn/checker_test.go
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2021-2025 Su Yang (soulteary)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package fn_test
 
 import (
